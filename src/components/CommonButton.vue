fa9a2ebb78b719eb762b794675c39461bbd65b04
Akshaytr <akshaytr31@gmail.com>
2025-07-04 15:49:10 +0530

    feat: Add common button component

---
+++ src/components/CommonButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-btn :class="['common-button', variant]" @click="handleEvent">
+      <v-icon v-if="icon">{{ icon }}</v-icon>
+      <span>{{ text }}</span>
+    </v-btn>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineEmits } from "vue";
+
+defineProps({
+  text: String,
+  variant: {
+    type: String,
+    default: "primary",
+  },
+  icon: String,
+});
+
+const emit = defineEmits(["onClick"]);
+
+const handleEvent = () => {
+  emit("onClick");
+};
+</script>
+<style lang="scss" scoped>
+.common-button {
+  border-radius: 30px;
+  font-size: 14px;
+  font-weight: 600;
+  text-transform: unset;
+  letter-spacing: unset;
+}
+
+.primary {  
+  background-color: #d1dff4;
+  color: #0e0e0e;
+}
+</style>
