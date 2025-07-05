3fb24917d02e5ce3bd61364643b46e6ba006c343
Akshaytr <akshaytr31@gmail.com>
2025-07-05 13:45:02 +0530

    feat:remove emit import

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -3,13 +3,20 @@
     <v-menu open-on-hover>
       <template v-slot:activator="{ props }">
         <div v-bind="props">
-          <v-icon class="icon">mdi-menu</v-icon>
+          <v-icon class="icon"> mdi-menu </v-icon>
         </div>
       </template>
-      <div style="width: 310px;">
+      <div style="width: 300px">
         <v-list>
-          <v-list-item v-for="(item, index) in items" :key="index" :value="index">
-            <router-link :to="`${item.path}`" style="text-decoration: none;color: black;">
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            :value="index"
+          >
+            <router-link
+              :to="`${item.path}`"
+              style="text-decoration: none; color: black"
+            >
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </router-link>
           </v-list-item>
@@ -19,10 +26,14 @@
   </div>
 </template>
 <script setup lang="ts">
-const items = [{ title: "Home",path:'/' }, { title: "Product",path:'/products' }, { title: "Cart",path:'/cart' }];
+const items = [
+  { title: "Home", path: "/" },
+  { title: "Product", path: "/products" },
+  { title: "Cart", path: "/cart" },
+];
 </script>
 <style lang="scss" scoped>
-.icon{
-    font-size: 30px;
+.icon {
+  font-size: 30px;
 }
 </style>
