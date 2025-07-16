faa06592a65437612202877f82024e59e32f93b8
Akshaytr <akshaytr31@gmail.com>
2025-07-16 10:39:07 +0530

    style: add style to loading

--- src/pages/EcommerceProductPage.vue
+++ src/pages/EcommerceProductPage.vue
@@ -1,9 +1,10 @@
 <template>
+  <div style="height: 82px;"></div>
   <div>
     <suspense>
       <CommonProducts #default />
       <template #fallback>
-        <div>Loading...</div>
+        <div class="loading-container">Loading...</div>
       </template>
     </suspense>
   </div>
@@ -11,3 +12,14 @@
 <script setup lang="ts">
 import CommonProducts from "@/components/CommonProducts.vue";
 </script>
+<style lang="scss" scoped>
+.loading-container{
+  font-size: 32px;
+  font-weight: 500;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
